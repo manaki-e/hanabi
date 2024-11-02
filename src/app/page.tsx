3fef844cb840740758cb58d6ae88439d8566896d
manaki-e <48690114s@gmail.com>
2024-11-02 20:23:06 +0900

    feat: create / display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@nextui-org/button';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Page() {
-  return <></>;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Image alt="hanabi" layout="fill" objectFit={'cover'} sizes="100vw" src="/hanabi.png" />
+      <Button as={Link} color="warning" href="/rooms" size="lg" variant="bordered">
+        ゲームへ移動
+      </Button>
+    </div>
+  );
 }
