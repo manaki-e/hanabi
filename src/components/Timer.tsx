b6a012a2c8f2563c5b84cbb52ac2df96feed6d23
manaki-e <48690114s@gmail.com>
2024-11-06 00:53:59 +0900

    fix: delete unnecessary request

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -36,7 +36,7 @@ export default function Timer({
       : uniqueNumbers[Math.floor(Math.random() * uniqueNumbers.length)];
 
   useEffect(() => {
-    if (timeLeft < 0) {
+    if (timeLeft == 0) {
       const formData = new FormData();
       if (teach_token === TEACH_TOKEN) {
         formData.append('form_id', 'hint');
@@ -60,13 +60,11 @@ export default function Timer({
       }
       setTimeout(() => {
         onOpenChange();
-        if (isOpen) {
-          window.location.reload();
-        }
+        window.location.reload();
       }, 3000);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timeLeft, hint, hintTypes, player_id, room_id, teach_token]);
+  }, [timeLeft]);
 
   return (
     <>
