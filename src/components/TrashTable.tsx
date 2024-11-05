3f53477f75a47b749e9640e15173b030250b2a58
manaki-e <48690114s@gmail.com>
2024-11-05 16:20:34 +0900

    fix: add total number in trash box

--- src/components/TrashTable.tsx
+++ src/components/TrashTable.tsx
@@ -2,13 +2,23 @@
 
 import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from '@nextui-org/table';
 
+import { CARD_NUMBERS } from '@/lib/constant';
 import { Color } from '@/lib/types';
 
 export default function TrashTable({ trash_table }: { trash_table: Record<Color, number[]> }) {
-  const getCellValue = (array: number[], index: number) => (index < array.length ? array[index] : 0);
+  const getCellValue = (array: number[], index: number) => {
+    const current = index < array.length ? array[index] : 0;
+    const total = CARD_NUMBERS[index];
+
+    return (
+      <>
+        <span className="text-sm font-bold">{current}</span>/{total}
+      </>
+    );
+  };
 
   return (
-    <Table aria-label="Example static collection table" classNames={{ base: 'text-xs' }} layout="fixed">
+    <Table aria-label="Example static collection table text-xs" layout="fixed">
       <TableHeader>
         <TableColumn align="center" className="w-8">
           {null}
@@ -33,11 +43,11 @@ export default function TrashTable({ trash_table }: { trash_table: Record<Color,
         {Array.from({ length: 5 }).map((_, rowIndex) => (
           <TableRow key={rowIndex}>
             <TableCell>{rowIndex + 1}</TableCell>
-            <TableCell className="text-blue-500">{getCellValue(trash_table.blue, rowIndex)}</TableCell>
-            <TableCell className="text-green-500">{getCellValue(trash_table.green, rowIndex)}</TableCell>
-            <TableCell className="text-red-500">{getCellValue(trash_table.red, rowIndex)}</TableCell>
-            <TableCell className="text-gray-500">{getCellValue(trash_table.white, rowIndex)}</TableCell>
-            <TableCell className="text-yellow-500">{getCellValue(trash_table.yellow, rowIndex)}</TableCell>
+            <TableCell className="text-xs text-blue-500">{getCellValue(trash_table.blue, rowIndex)}</TableCell>
+            <TableCell className="text-xs text-green-500">{getCellValue(trash_table.green, rowIndex)}</TableCell>
+            <TableCell className="text-xs text-red-500">{getCellValue(trash_table.red, rowIndex)}</TableCell>
+            <TableCell className="text-xs text-gray-500">{getCellValue(trash_table.white, rowIndex)}</TableCell>
+            <TableCell className="text-xs text-yellow-500">{getCellValue(trash_table.yellow, rowIndex)}</TableCell>
           </TableRow>
         ))}
       </TableBody>
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,2 +1,3 @@
 export const TEACH_TOKEN = 8;
 export const MISS_TOKEN = 3;
+export const CARD_NUMBERS = [3, 2, 2, 2, 1];
