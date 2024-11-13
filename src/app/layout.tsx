c14750914ccad8edb9ffd677e7e7901cc824d354
manaki-e <48690114s@gmail.com>
2024-11-13 22:57:42 +0900

    fix: change language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={font.className}>
         <main className="h-screen w-screen overflow-hidden bg-gray-100">{children}</main>
       </body>
