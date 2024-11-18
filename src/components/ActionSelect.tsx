fea21c1e9c6ebecaaf58cd40f84cb36668240724
manaki-e <48690114s@gmail.com>
2024-11-18 14:32:02 +0900

    fix: short time in thinking

--- src/components/ActionSelect.tsx
+++ src/components/ActionSelect.tsx
@@ -47,7 +47,7 @@ export default function ActionSelect({
         () => {
           submitData(event, room_id, player_id, formData, elapsed_time).finally(() => setIsLoading(false));
         },
-        (timeLeft - 2) * 1000,
+        (timeLeft - 10) * 1000,
       );
     } else {
       submitData(event, room_id, player_id, undefined, elapsed_time).finally(() => setIsLoading(false));
@@ -87,7 +87,7 @@ export default function ActionSelect({
             color="primary"
             label={
               Number(room_id) >= 100 && Number(room_id) < 150
-                ? `${timeLeft}秒後に送信されます。そのままお待ちください。`
+                ? `${timeLeft > 10 ? timeLeft - 10 : 0}秒後に送信されます。そのままお待ちください。`
                 : ''
             }
             size="lg"
